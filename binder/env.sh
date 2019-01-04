bd891951e312cb48a2737a4c85174886a94f734c
slejdops <slejdops@gmail.com>
2019-01-04 11:24:14 +0000

    added env.sh

---
+++ binder/env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# add miniconda to the path
+export PATH=$(pwd)/binder/deps/conda/bin:/home/jovan:$PATH
+
+# add texlive to the path
+if [ "$(uname)" == "Darwin" ]; then
+    export PATH=$(pwd)/binder/deps/texlive/bin/x86_64-darwin:$PATH
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    export PATH=$(pwd)/binder/deps/texlive/bin/x86_64-linux:$PATH
+fi
+
+# determine name of current directory, use as conda environment name
+CONDANAME=${PWD##*/}
+
+# activate conda environment
+source activate $CONDANAME
